f7750da119110f9205130c4770bab5869b227a90
ahmed-H20 <ahmedheshamahah8@gmail.com>
2025-09-19 18:33:03 +0300

    Uptodate all Chanfes

--- src/components/ads/BannerAdd.jsx
+++ src/components/ads/BannerAdd.jsx
@@ -2,31 +2,32 @@ import { useEffect } from "react";
 
 export default function BannerAd() {
   useEffect(() => {
-    // set global variable
-    window.atOptions = {
-      key: "68d870f6fb9497091b5abd54fb292cd5",
-      format: "iframe",
-      height: 60,
-      width: 468,
-      params: {},
-    };
+    const script1 = document.createElement("script");
+    script1.type = "text/javascript";
+    script1.innerHTML = `
+      atOptions = {
+        'key' : '68d870f6fb9497091b5abd54fb292cd5',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+      };
+    `;
+    const script2 = document.createElement("script");
+    script2.type = "text/javascript";
+    script2.src = "//www.highperformanceformat.com/68d870f6fb9497091b5abd54fb292cd5/invoke.js";
 
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src =
-      "//www.highperformanceformat.com/68d870f6fb9497091b5abd54fb292cd5/invoke.js";
-    script.async = true;
-
-    const banner = document.getElementById("banner-ad");
-    if (banner) {
-      banner.appendChild(script);
-    }
+    document.getElementById("banner-ad").appendChild(script1);
+    document.getElementById("banner-ad").appendChild(script2);
   }, []);
 
   return (
-      <div
-        id="banner-ad"
-        style={{ textAlign: "center", margin: "20px 0" }}
-      />
+    <>
+      <h1>Banner Ad small</h1>
+      <div id="banner-ad" style={{ textAlign: "center", margin: "20px 0" }}>
+        {/* الإعلان هيتحمل هنا */}
+      </div>
+    </>
+    
   );
 }
