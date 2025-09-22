7152f4c2a278d38cc452c0649834f6bc64a53438
ahmed-H20 <ahmedheshamahah8@gmail.com>
2025-09-23 00:48:05 +0300

    Update banner ad script key and source URL for improved integration

--- src/components/ads/BannerAdd.jsx
+++ src/components/ads/BannerAdd.jsx
@@ -6,16 +6,16 @@ export default function BannerAd() {
     script1.type = "text/javascript";
     script1.innerHTML = `
       atOptions = {
-        'key' : 'a94bf5be7147514dd1086e4d1e7c1ef0',
+        'key' : 'af65b1c1a249a861126c74a2a35dc488',
         'format' : 'iframe',
-        'height' : 60,
-        'width' : 468,
+        'height' : 50,
+        'width' : 320,
         'params' : {}
       };
     `;
     const script2 = document.createElement("script");
     script2.type = "text/javascript";
-    script2.src = "//www.highperformanceformat.com/a94bf5be7147514dd1086e4d1e7c1ef0/invoke.js";
+    script2.src = "//www.highperformanceformat.com/af65b1c1a249a861126c74a2a35dc488/invoke.js";
 
     document.getElementById("banner-ad").appendChild(script1);
     document.getElementById("banner-ad").appendChild(script2);
@@ -26,7 +26,6 @@ export default function BannerAd() {
       <div id="banner-ad" style={{ textAlign: "center", margin: "20px 0" }}>
         {/* الإعلان هيتحمل هنا */}
       </div>
-    </>
-    
+    </>    
   );
 }
